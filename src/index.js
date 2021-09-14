ea7c88baa71a9fcc7efc1ee64d05f5ac3bff529e
phamthao80s <phamthao80s@gmail.com>
2021-09-14 10:56:30 +0100

    Changing content in Index.js using JSX, update index.css file
    Changing content in Index.js using JSX, update index.css file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
-const myfirstelement = <h1>Hello React!</h1>
+const user ={ 
+    name: "Thao Pham",
+    email: "phamthao@80sgmail.com",
+};
 
+const myfirstelement =(
+<div className="Hello">
+<p> <h1> Hello Every one, </h1>
+
+<h3> My name is: {user.name}, and my email is: {user.email}</h3></p>
+
+    <h5> If you wanna contact with {user.name}, pls send me email to: {user.email} </h5>
+</div>
+);
 ReactDOM.render(myfirstelement, document.getElementById('root'));
 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 
