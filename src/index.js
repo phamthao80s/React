27d137ae9e32bd69b53ff3f0ffc6462a8fd2a7cb
phamthao80s <phamthao80s@gmail.com>
2021-09-14 11:05:05 +0100

    Changing content of Index.js file
    Changing content of Index.js file. Using arrow function.

--- src/index.js
+++ src/index.js
@@ -6,17 +6,18 @@ const user ={
     name: "Thao Pham",
     email: "phamthao@80sgmail.com",
 };
+// const myfirstelement =(
+// <div className="Hello">
+// <p> <h1> Hello Every one, </h1>
 
-const myfirstelement =(
-<div className="Hello">
-<p> <h1> Hello Every one, </h1>
+// <h3> My name is: {user.name}, and my email is: {user.email}</h3></p>
 
-<h3> My name is: {user.name}, and my email is: {user.email}</h3></p>
-
-    <h5> If you wanna contact with {user.name}, pls send me email to: {user.email} </h5>
-</div>
-);
-ReactDOM.render(myfirstelement, document.getElementById('root'));
+//     <h5> If you wanna contact with {user.name}, pls send me email to: {user.email} </h5>
+// </div>
+// );
+var numbers=[1,2,3,4];
+var doubleNumbers=numbers.map((eachNumber)=>eachNumber*2+";");
+ReactDOM.render(<h1>doubleNumbers is: {doubleNumbers} </h1>, document.getElementById('root'));
 
 
 
