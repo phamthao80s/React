3530190a4bd4c3d4ee87b2ea2fc32f60910ce175
phamthao80s <phamthao80s@gmail.com>
2021-09-15 10:48:37 +0100

    add Component and props/ part 1
    Define component as JS function and class.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import reactDom from 'react-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
 // #region 1
@@ -7,7 +8,7 @@ import './index.css';
 //     email: "phamthao@80sgmail.com",
 // };
 // #endregion 1
-//#region 2
+//#region 2 JSX
 // const myfirstelement =(
 // <div className="Hello">
 // <p> <h1> Hello Every one, </h1>
@@ -20,23 +21,53 @@ import './index.css';
 //ReactDOM.render(myfirstelement, document.getElementById('root'));
 
 //#endregion 2
-//#region 3
+//#region 3 Arrow function
 // var numbers=[1,2,3,4];
 // var doubleNumbers=numbers.map((eachNumber)=>eachNumber*2+";");
 // ReactDOM.render(<h1>Double Numbers is: {doubleNumbers} </h1>, document.getElementById('root'));
 //#endregion 3
-//#region 4
-function updateTimer(){
-    const element =(
-        <div>
-            <h1> Timer update example</h1>
-            <h2> Current time now is: {new Date().toLocaleTimeString()}</h2>
-        </div>
-    );
-    ReactDOM.render(element, document.getElementById('root')
-    );
-}
-setInterval(updateTimer,1000);
+//#region 4 Rendering element
+// function updateTimer(){
+//     const element =(
+//         <div>
+//             <h1> Timer update example</h1>
+//             <h2> Current time now is: {new Date().toLocaleTimeString()}</h2>
+//         </div>
+//     );
+//     ReactDOM.render(element, document.getElementById('root')
+//     );
+// }
+// setInterval(updateTimer,1000);
 //#endregion 4
-
-
+//#region 5 Component and Props
+// // define component as js function
+// function Userinfo(props){
+//     return (
+//         <div>
+//             <p>Name: {props.name}</p>
+//             <p> Email: {props.emai}</p>
+//         </div>
+//     );
+//}
+// define Component as class
+class Userinfo extends React.Component{
+    render(){
+        return(
+            <div>
+            <div className ="UserDetail">
+                 <p>Name: {this.props.name}</p>
+                 <p> Email: {this.props.emai}</p>
+            </div>
+            <div className="OtherInfo">
+                <p> Other Information: {this.props.otherInformation}</p>
+            </div>
+            </div>
+        );
+    }
+}
+const element = <Userinfo name ="Thao Pham" emai ="cindypham88@gmail.com" otherInformation="I like working from Home"/>;
+ReactDOM.render(
+    element,
+    document.getElementById('root')
+);
+//#endregion 5
