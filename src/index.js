0c06742e7b5ab11e52f570d37f65250e90412042
phamthao80s <phamthao80s@gmail.com>
2021-09-20 12:24:22 +0100

    State

--- src/index.js
+++ src/index.js
@@ -89,41 +89,73 @@ import './index.css';
 //     document.getElementById('root')
 // );
 //#endregion 5 Component and Props
-//#region 6 State and Lifecycle
-class Counter extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state ={seconds:0};
-    }
-    incrementCounter(){
-        this.setState(
-            (
-                prevSate,props)=>({
-                    seconds: prevSate.seconds +1
+// //#region 6 State and Lifecycle
+// class Counter extends React.Component{
+//     constructor(props) {
+//         super(props);
+//         this.state ={seconds:0};
+//     }
+//     incrementCounter(){
+//         this.setState(
+//             (
+//                 prevSate,props)=>({
+//                     seconds: prevSate.seconds +1
 
-                })
-            );
-    }
-    componentDidMount (){
-        this.timerID = setInterval(
-            ()=> this.incrementCounter(),
-            1000
-        );
+//                 })
+//             );
+//     }
+//     componentDidMount (){
+//         this.timerID = setInterval(
+//             ()=> this.incrementCounter(),
+//             1000
+//         );
 
-    }
-    componentWillUnmount(){
-        clearInterval(this.timerID);
+//     }
+//     componentWillUnmount(){
+//         clearInterval(this.timerID);
 
-    }
-    render(){
-        return(
-            <div>
-                <h1> This is a counting machine</h1>
-                <h2> Seconds: {this.state.seconds} s </h2>
-            </div>
-        );
-    }
-    }
-ReactDOM.render(<Counter/>,document.getElementById ('root')
-);
-//#endregion
+//     }
+//     render(){
+//         return(
+//             <div>
+//                 <h1> This is a counting machine</h1>
+//                 <h2> Seconds: {this.state.seconds} s </h2>
+//             </div>
+//         );
+//     }
+//     }
+// ReactDOM.render(<Counter/>,document.getElementById ('root')
+// );
+// //#endregion
+//#region 7 More State
+// class Car extends React.Component {
+//     constructor(props){
+//         super(props);
+//         this.state ={
+//             brand: "ford",
+//             model: "mj",
+//             color: "red",
+//             years: 1967
+//         };
+//     }
+//     changeColor = ()=>{
+//         this.setState({color:"blue"});
+// }
+//     render(){
+       
+//         return (
+//         <div>
+//         <h2> i am a {this.state.brand} car</h2>
+//         <p>
+//         it is a {this.state.color} {this.state.model} from {this.state.years}
+//         </p>
+//         <button
+//         type ="button"
+//         onClick ={this.changeColor} > changeColor</button>
+//         </div>
+//         );
+//     }
+// }
+
+// ReactDOM.render(<Car/>, document.getElementById('root'));
+//#endregion 7
